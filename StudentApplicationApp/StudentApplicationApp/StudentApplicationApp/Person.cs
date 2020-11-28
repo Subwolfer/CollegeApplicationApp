2e3833e99b342629b66c426467dbe3aa35ac71d8
Michel Koopman <subwolfer@hotmail.com>
2020-11-27 23:28:24 -0500

    Fixing person.cs bug

---
+++ StudentApplicationApp/StudentApplicationApp/StudentApplicationApp/Person.cs
@@ -0,0 +1,71 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StudentApplicationApp.Persistence
+{
+    public class Person : IPerson
+    {
+        [PrimaryKey, Unique, MaxLength(255)]
+        public string Email { get; set; }
+
+        [MaxLength(255)]
+        public string Password { get; set; }
+
+        public bool isStaff { get; set; }
+
+        /// <summary>
+        /// 0 arg constructor required for SQLite connection
+        /// </summary>
+        public Person()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor for setting up a nwe account.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        public Person(string email, string password)
+        {
+            Email = email;
+            Password = password;
+            isStaff = false; // false by default
+
+            OnAdd();
+        }
+
+        public void SetPersonAsStaff()
+        {
+            isStaff = true;
+            var connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+            connection.UpdateAsync(this);
+        }
+
+        public async void OnAdd()
+        {
+            var connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+
+            await connection.InsertAsync(this);
+        }
+
+        /// <summary>
+        /// Virtual funciton. Not implemented here.
+        /// </summary>
+        public virtual void CreateAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Virtual funciton. Not implemented here.
+        /// </summary>
+        public virtual void Log_In()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StudentApplicationApp/StudentApplicationApp/StudentApplicationApp/StudentApplication.cs
@@ -0,0 +1,62 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApplicationApp
+{
+    class StudentApplication
+    {
+        [PrimaryKey]
+        public string Email { get; set; }
+
+        [MaxLength(255)]
+        public string FirstName { get; set; }
+
+        [MaxLength(255)]
+        public string LastName { get; set; }
+
+        [MaxLength(255)]
+        public string Street { get; set; }
+
+        [MaxLength(255)]
+        public string City { get; set; }
+
+        [MaxLength(20)]
+        public string State { get; set; }
+
+        [MaxLength(10)]
+        public string Zip { get; set; }
+
+        /// <summary>
+        /// 0 arg constructor required for SQLite connection
+        /// </summary>
+        public StudentApplication()
+        {
+
+        }
+
+        /// <summary>
+        /// Full constructor for saving data to SQLite
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <param name="first"></param>
+        /// <param name="last"></param>
+        /// <param name="street"></param>
+        /// <param name="city"></param>
+        /// <param name="state"></param>
+        /// <param name="zip"></param>
+        public StudentApplication(string email, string password, string first, string last, string street, string city, string state, string zip)
+        {
+            Email = email;
+            FirstName = first;
+            LastName = last;
+            Street = street;
+            City = city;
+            State = state;
+            Zip = zip;
+        }
+
+    }
+}
